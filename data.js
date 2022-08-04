a2b6720a88d922314dbd2b5f6dd59ee1e04b4b30
Michal <michalgrzegorczyk04@gmail.com>
2022-08-04 15:01:54 +0200

    Create Rest Api

---
+++ data.js
@@ -0,0 +1,42 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Kajak",
+        category: "Sporty wodne",
+        description: "Łódka przeznaczona dla jednej osoby",
+        price: 275,
+      },
+      {
+        id: 2,
+        name: "Kamizelka ratunkowa",
+        category: "Sporty wodne",
+        description: "Chroni i dodaje uroku",
+        price: 48.95,
+      },
+      {
+        id: 3,
+        name: "Czapka",
+        category: "Szachy",
+        description: "Zwiększa efektywność mózgu o 75%",
+        price: 16,
+      },
+      {
+        id: 4,
+        name: "Niestabilne krzesło",
+        category: "Szachy",
+        description: "Zmniejsza szanse przeciwnika",
+        price: 29.25,
+      },
+      {
+        id: 5,
+        name: "Błyszczący król",
+        category: "Szachy",
+        description: "Pokryty złotem i wysadzany diamentami król",
+        price: 1200,
+      },
+    ],
+    orders: [],
+  };
+};
