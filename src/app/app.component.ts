3cd1bb2cbc48754f30b0aa3195ca5a2653a9c279
Michal <michalgrzegorczyk04@gmail.com>
2022-08-04 18:44:01 +0200

    Add store component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { ProductRepository } from './model/product.repository';
 
 @Component({
   selector: 'app-store',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<store></store>`,
 })
 export class AppComponent {
   title = 'SportsStore';
