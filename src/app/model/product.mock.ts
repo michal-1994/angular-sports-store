2a6aeab15a51b1e50e4a6e1138daed942bd1fc1a
Michal <michalgrzegorczyk04@gmail.com>
2022-08-05 11:06:05 +0200

    Add category selection

--- src/app/model/product.mock.ts
+++ src/app/model/product.mock.ts
@@ -6,10 +6,10 @@ import { Product } from "./product.model";
 export class ProductData {
     private products: Product[] = [
         new Product(1, "Produkt 1", "Kategoria 1", "Produkt 1 (Kategoria 1)", 100),
-        new Product(2, "Produkt 1", "Kategoria 1", "Produkt 1 (Kategoria 1)", 100),
-        new Product(3, "Produkt 1", "Kategoria 1", "Produkt 1 (Kategoria 1)", 100),
-        new Product(4, "Produkt 1", "Kategoria 1", "Produkt 1 (Kategoria 1)", 100),
-        new Product(5, "Produkt 1", "Kategoria 1", "Produkt 1 (Kategoria 1)", 100)
+        new Product(2, "Produkt 2", "Kategoria 2", "Produkt 1 (Kategoria 2)", 100),
+        new Product(3, "Produkt 3", "Kategoria 2", "Produkt 1 (Kategoria 2)", 100),
+        new Product(4, "Produkt 4", "Kategoria 1", "Produkt 1 (Kategoria 1)", 100),
+        new Product(5, "Produkt 5", "Kategoria 3", "Produkt 1 (Kategoria 3)", 100)
     ];
 
     getProducts(): Observable<Product[]> {
