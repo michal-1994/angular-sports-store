9ebfbb57b0c9fd226edcd9d367503b4746bd859e
Michal <michalgrzegorczyk04@gmail.com>
2022-08-05 10:57:46 +0200

    Add product cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { registerLocaleData } from '@angular/common';
+import localePL from '@angular/common/locales/pl';
+registerLocaleData(localePL, 'pl');
 
 @NgModule({
   declarations: [
