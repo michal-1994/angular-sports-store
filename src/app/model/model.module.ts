78a79fba84f03a815842397a0f511c4ff296dcee
Michal <michalgrzegorczyk04@gmail.com>
2022-08-06 17:47:09 +0200

    Create cart model

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
+import { Cart } from "./cart.model";
 import { ProductData } from "./product.mock";
 import { ProductRepository } from "./product.repository";
 
 @NgModule({
-    providers: [ProductRepository, ProductData]
+    providers: [ProductRepository, ProductData, Cart]
 })
 export class ModelModule {}
