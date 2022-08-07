19c879feb31a4bc63ed5af9fca3a145ab408c491
Michal <michalgrzegorczyk04@gmail.com>
2022-08-07 06:45:49 +0200

    Add route guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RouterModule } from '@angular/router';
 import { StoreComponent } from './store/store.component';
 import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 registerLocaleData(localePL, 'pl');
 
@@ -23,20 +24,20 @@ registerLocaleData(localePL, 'pl');
     BrowserAnimationsModule,
     RouterModule.forRoot([
       {
-        path: 'store', component: StoreComponent
+        path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard]
       },
       {
-        path: 'cart', component: CartDetailComponent
+        path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard]
       },
       {
-        path: 'checkout', component: CheckoutComponent
+        path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard]
       },
       {
         path: '**', redirectTo: './store'
       }
     ])
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
