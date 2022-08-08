59eca584ca4bf08e50e1b951416d0767e3e13881
Michal <michalgrzegorczyk04@gmail.com>
2022-08-08 08:54:53 +0200

    Create admin and auth components

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { AdminComponent } from "./admin.component";
+import { AuthComponent } from "./auth.component";
+
+let routing = RouterModule.forChild([
+    { path: 'auth', component: AuthComponent },
+    { path: 'main', component: AdminComponent },
+    { path: '**', redirectTo: 'auth' },
+]);
+
+@NgModule({
+    imports: [CommonModule, FormsModule, routing],
+    declarations: [AdminComponent, AuthComponent]
+})
+export class AdminModule { }
