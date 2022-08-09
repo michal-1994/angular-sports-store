1bd429d6eb0eba06af20153b45ab7868802d49c5
Michal <michalgrzegorczyk04@gmail.com>
2022-08-09 08:49:36 +0200

    Prepare global admin panel

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,16 +5,27 @@ import { RouterModule } from "@angular/router";
 import { AdminComponent } from "./admin.component";
 import { AuthComponent } from "./auth.component";
 import { AuthGuard } from "./auth.guard";
+import { OrderTableComponent } from "./orderTable.component";
+import { ProductEditorComponent } from "./productEditor.component";
+import { ProductTableComponent } from "./productTable.component";
 
 let routing = RouterModule.forChild([
     { path: 'auth', component: AuthComponent },
-    { path: 'main', component: AdminComponent, canActivate: [AuthGuard] },
+    {
+        path: 'main', component: AdminComponent, canActivate: [AuthGuard], children: [
+            { path: 'products/:mode/:id', component: ProductEditorComponent },
+            { path: 'products/:mode', component: ProductEditorComponent },
+            { path: 'products', component: ProductTableComponent },
+            { path: 'orders', component: OrderTableComponent },
+            { path: '**', redirectTo: 'products' }
+        ]
+    },
     { path: '**', redirectTo: 'auth' },
 ]);
 
 @NgModule({
     imports: [CommonModule, FormsModule, routing],
-    declarations: [AdminComponent, AuthComponent],
+    declarations: [AdminComponent, AuthComponent, ProductTableComponent, ProductEditorComponent, OrderTableComponent],
     providers: [AuthGuard]
 })
 export class AdminModule { }
